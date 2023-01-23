ca45610dd90f41d6884d3494d006bb32915b1997
Anderson Rosero <andersoncamilo132@gmail.com>
2023-01-23 18:37:43 -0500

    fusionando el mobile-menu

--- fusion.js
+++ fusion.js
@@ -1,8 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const dskMenu = document.querySelector(".dsk-menu");
+const imgMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+imgMenu.addEventListener("click", toggleMobileMenu);
+
 
 function toggleDesktopMenu(){
     dskMenu.classList.toggle("inactive");
 }
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
+}
+
