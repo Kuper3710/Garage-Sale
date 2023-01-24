296b3df99fa48a02e0b1542b502bec55b6694d90
Anderson Rosero <andersoncamilo132@gmail.com>
2023-01-24 16:34:26 -0500

    fusion carrito-compras con index.html

--- fusion.js
+++ fusion.js
@@ -2,16 +2,62 @@ const menuEmail = document.querySelector(".navbar-email");
 const dskMenu = document.querySelector(".dsk-menu");
 const imgMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const shoppingCard = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 imgMenu.addEventListener("click", toggleMobileMenu);
+shoppingCard.addEventListener("click", toggleProdcutDetail);
 
 
 function toggleDesktopMenu(){
-    dskMenu.classList.toggle("inactive");
+    const isProductDetail = productDetail.classList.contains("inactive");
+
+    if (!isProductDetail){
+        productDetail.classList.add("inactive");
+        dskMenu.classList.toggle("inactive");
+    } else{
+        dskMenu.classList.toggle("inactive");
+    }
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle("inactive");
+    const isProductDetail = productDetail.classList.contains("inactive");
+
+    if(isProductDetail){
+        mobileMenu.classList.toggle("inactive");
+    } else if(!isProductDetail){
+        productDetail.classList.add("inactive");
+        mobileMenu.classList.toggle("inactive");
+    }
 }
 
+function toggleProdcutDetail(){
+    const isMobileMenu = mobileMenu.classList.contains("inactive");
+    const isDskMenu = dskMenu.classList.contains("inactive");
+
+    if(isDskMenu){
+        if(isMobileMenu){
+            productDetail.classList.toggle("inactive");
+        } else if(!isMobileMenu){
+                mobileMenu.classList.toggle("inactive");
+                productDetail.classList.toggle("inactive");
+            } 
+        else{
+            productDetail.classList.toggle("inactive");
+        }
+        
+    } else if(!isDskMenu){
+        dskMenu.classList.add("inactive");
+        productDetail.classList.toggle("inactive");
+    } 
+}
+
+// const isMobileMenu = mobileMenu.classList.contains("inactive");
+
+// if(isMobileMenu){
+//     productDetail.classList.toggle("inactive");
+// } else if(!isMobileMenu){
+//     mobileMenu.classList.toggle("inactive");
+//     productDetail.classList.toggle("inactive");
+// } 
